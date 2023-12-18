ee095181712ba57415acfe23bf3ce8d9ad71684c
10nm <10nm@protonmail.com>
2023-12-18 01:21:04 +0000

    フォルダーに移行

---
+++ source/bek_load.py
@@ -0,0 +1,58 @@
+import random
+import json
+import glob
+from tqdm import tqdm
+import numpy as np
+from sklearn.manifold import TSNE
+from sklearn.decomposition import PCA
+import torch
+from torch.utils.data import Dataset, DataLoader
+from transformers import BertJapaneseTokenizer, BertForMaskedLM
+from sentence_transformers import SentenceTransformer, util
+import re
+import pickle
+import os
+
+i = 0
+
+os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "max_split_size_mb:4096"
+
+model = SentenceTransformer('sentence-transformers/paraphrase-xlm-r-multilingual-v1')
+
+with open('wk.pkl', "rb") as fIn:
+    stored_data = pickle.load(fIn)
+    stored_sentences = stored_data['sentences']
+    stored_embeddings = stored_data['embeddings']
+    stored_ids = stored_data['id']
+    print("file loaded")
+
+    query = "愛知県の知事"
+    query_embedding = model.encode(query, convert_to_tensor=True)
+    print("query embedded")
+
+    with open('wktrc.pkl', 'rb') as f:
+        emb = pickle.load(f)
+        print("emb loaded")
+
+    print(type(emb))
+    cos_scores = util.cos_sim(query_embedding, emb)[0]
+    top_results = torch.topk(cos_scores, k=10)
+
+    print((stored_sentences[0])[:50])
+    print("https://ja.wikipedia.org/w/index.php?curid=" + str(stored_ids[0]))
+
+    for score, idx in zip(top_results[0], top_results[1]):
+        i += 1
+        print(str(i), ":\n")
+
+        print("score: ")
+        print(score, "\n")
+
+        print("id: ")
+        print(stored_ids[idx], "\n")
+
+        print("text: ")
+        print((stored_sentences[idx])[:50], "\n")
+
+        print("wiki page:")
+        print("https://ja.wikipedia.org/w/index.php?curid=" + str(stored_ids[idx]))
